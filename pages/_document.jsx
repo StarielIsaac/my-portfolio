fa995c27651bde5e996eb22b60dfc73a8ccd0ea3
Stariel Isaac <starielisaac@gmail.com>
2023-06-27 21:36:27 -0300

    feat: created _document.jsx

---
+++ pages/_document.jsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Inter"
+          />
+        </Head>
+        <body className="bg-gray-100">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
